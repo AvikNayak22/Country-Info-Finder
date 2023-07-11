76e609b663fed0bddb67d3c6b0d9ecf126d35a1b
AvikNayak <110925067+AvikNayak22@users.noreply.github.com>
2023-07-11 12:56:37 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,19 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Countries from "./components/Countries";
 import SingleCountry from "./components/SingleCountry";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Countries />}></Route>
-        <Route path="/:name" element={<SingleCountry />}></Route>
-      </Routes>
-    </BrowserRouter>
+    <>
+      <Navbar />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Countries />}></Route>
+          <Route path="/:name" element={<SingleCountry />}></Route>
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 }
 
